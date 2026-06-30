de601155e6cfc4283c5bb9c268f49e5eaeeb93fb
NirupamChakraborty <nirupamchakraborty04@gmail.com>
2026-06-30 17:33:44 +0530

    added new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,24 @@ import { Toaster } from 'react-hot-toast'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import Authentication from './pages/Authentication'
-import Landing from './pages/Landing'
 import VideoMeetComponent from './pages/VideoMeet'
 
 const App = () => {
   return (
     <div>
+
+      
         <Router>
         <Toaster position="top-center" reverseOrder={true}/>
           <AuthProvider >
             <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/auth" element={<Authentication />} />
+                  <Route path='/' element={<LandingPage />} />
 
-                <Route path="/:url" element={<VideoMeetComponent />} />
+                   <Route path='/auth' element={<Authentication />} />
+
+                    <Route path='/home's element={<HomeComponent />} />
+                    <Route path='/history' element={<History />} />
+                      <Route path='/:url' element={<VideoMeetComponent />} />
             </Routes>
             </AuthProvider>
         </Router>
